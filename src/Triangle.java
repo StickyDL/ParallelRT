f535562bc4952881d00872753460ffcfe4f4ff2e
Steven Glazer <sfg6126@rit.edu>
2012-01-20 16:51:55 -0500

    Changed constructors and added descritpions so we have more control over triangles along with shader control

--- src/Triangle.java
+++ src/Triangle.java
@@ -7,29 +7,60 @@ public class Triangle extends GraphicObject {
 	ArrayList<Point3d> vertices;
 	Shader shader = null;
 
-	public Triangle(ArrayList<Point3d> vertices, Vector3d normal, Color ambc) {
+	public Triangle(ArrayList<Point3d> vertices, Vector3d normal, Color ambc)
+	{
+        this(vertices, normal, ambc, 0.2, 0.4, 0.6, 20, 0.0, 0.0, 0);
+    }
+    
+    public Triangle(ArrayList<Point3d> vertices, Vector3d normal, Color ambc, int shaderIndex)
+    {
+        this(vertices, normal, ambc, 0.2, 0.4, 0.6, 20, 0.0, 0.0, shaderIndex);
+    }
 
+    /*
+	    Creates a Sphere object with the given vertices, surface normal, ambient color,
+	    ambient light, diffuse light, specular light, the exponent, reflectivity,
+	    transparency, and a shaderIndex.
+	    
+	    ka - Ambient Lighting (0-1). Amount of background light
+	    kd - Diffuse Lighting (0-1). Lambertian Reflection
+	    ks - Specular Lighting (0-1). Mirror-like Reflection
+	    ke - Exponent (20-100. Needs to be EVEN) - Controls size of Specular Highlight
+	    kr - Reflectivity (0-1). 0=Non-Reflective 1=Reflective
+	    kt - Transparency (0-1). 0=Non-Transparent 1=Transparent
+	    shaderIndex - 0=No Shader 1=Checkerboard 2=PolkaDot 3=Ripple
+	*/
+    public Triangle(ArrayList<Point3d> vertices, Vector3d normal, Color ambc, double ka, double kd, double ks, int ke, double kr, double kt, int shaderIndex)
+	{
 		this.vertices = vertices;
 		this.normal = normal;
 		this.ambcolor = ambc;
-		ka = 0.2; //.4
-		kd = 0.4; //.3
-		ks = 0.6; //.4
-		ke = 20;
-		kr = 0.0;
-		kt = 0.0;
-		//Checker Board Shader
-		shader = new CheckBoardShad(this.vertices.get(1), new Color(255.0, 0.0, 0.0), 
-												new Color(255.0, 255.0, 0.0), 1.5);
+		this.ka = ka;
+		this.kd = kd;
+		this.ks = ks;
+        if ( (ke & 1) == 0 ) {
+            this.ke = (double)ke;
+        } else {
+            this.ke = (double)ke+1.0;
+        }
+		this.kr = kr;
+		this.kt = kt;
 		
-		//Polka Dot Shader
-		//shader = new PolkaDotShad(new Point3d(-2.25, 0.0, 0.0), new Point3d(1.25, 0.0, -10.0), 
-		//							new Color(50.0, 100.0, 200.0), new Color(255.0, 255.0, 0.0),
-		//							new Color(200.0, 0.0, 0.0), 0.6, 0.25);
-		
-		//Ripple Shader
-		//shader = new RippleShad(new Point3d(0.0, 0.0, -5.0));
-}
+		if ( shaderIndex == 1 ) {
+    		//Checker Board Shader
+            this.shader = new CheckBoardShad(this.vertices.get(1), new Color(255.0, 0.0, 0.0), 
+                                                    new Color(255.0, 255.0, 0.0), 1.5);
+		} else if ( shaderIndex == 2 ) {
+    		//Polka Dot Shader
+            this.shader = new PolkaDotShad(new Point3d(-2.25, 0.0, 0.0), new Point3d(1.25, 0.0, -10.0), 
+                                        new Color(50.0, 100.0, 200.0), new Color(255.0, 255.0, 0.0),
+                                        new Color(200.0, 0.0, 0.0), 0.6, 0.25);
+        } else if ( shaderIndex == 3 ) {
+    		//Ripple Shader
+            this.shader = new RippleShad(new Point3d(0.0, 0.0, -5.0));
+        }
+    }
+
 
 	public Point3d intersect(Ray r) {
 
