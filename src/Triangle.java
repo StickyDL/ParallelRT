8043e0c218c49365d55f457d1674c25b650420eb
Steven Glazer <sfg6126@rit.edu>
2012-02-01 11:35:11 -0500

    Fixed Intersection code - NO MORE TOP REFLECTIONS. Plus reflection and transmission on triangles

--- src/Triangle.java
+++ src/Triangle.java
@@ -14,7 +14,7 @@ public class Triangle extends GraphicObject {
     
     public Triangle(ArrayList<Point3d> vertices, Vector3d normal, Color ambc, int shaderIndex)
     {
-        this(vertices, normal, ambc, 0.2, 0.4, 0.6, 20, 0.0, 0.0, shaderIndex);
+        this(vertices, normal, ambc, 0.2, 0.4, 0.6, 20, 0.6, 0.0, shaderIndex);
     }
 
     /*
@@ -48,8 +48,8 @@ public class Triangle extends GraphicObject {
 		
 		if ( shaderIndex == 1 ) {
     		//Checker Board Shader
-            this.shader = new CheckBoardShad(this.vertices.get(1), new Color(255.0, 0.0, 0.0), 
-                                                    new Color(255.0, 255.0, 0.0), 1.5);
+            this.shader = new CheckBoardShad(this.vertices.get(1), new Color(0.0, 0.0, 0.0), 
+                                                    new Color(500.0, 500.0, 500.0), 1.5);
 		} else if ( shaderIndex == 2 ) {
     		//Polka Dot Shader
             this.shader = new PolkaDotShad(new Point3d(-2.25, 0.0, 0.0), new Point3d(1.25, 0.0, -10.0), 
@@ -62,72 +62,33 @@ public class Triangle extends GraphicObject {
     }
 
 
-	public Point3d intersect(Ray r) {
-
-		//http://www.devmaster.net/wiki/Ray-triangle_intersection
-		//http://www.lighthouse3d.com/opengl/maths/index.php?raytriint
-		//http://www.cs.virginia.edu/~gfx/Courses/2003/ImageSynthesis/papers/Acceleration/Fast%20MinimumStorage%20RayTriangle%20Intersection.pdf
-
-		Vector3d edge1, edge2, tvec;
-		Vector3d pvec = new Vector3d();
-		Vector3d qvec = new Vector3d();
-
-		double det, inv_det, u, v, t;
-		final double EPSILON = 0.000001;
-
-		//find vectors for two edges sharing vertex 0
-
-		edge1 = new Vector3d(vertices.get(1).x - vertices.get(0).x, vertices.get(1).y - vertices.get(0).y,
-				vertices.get(1).z - vertices.get(0).z);
-		edge2 = new Vector3d(vertices.get(2).x - vertices.get(0).x, vertices.get(2).y - vertices.get(0).y,
-				vertices.get(2).z - vertices.get(0).z);
-
-		//begin calculation determinant
-
-		pvec.cross(r.direction, edge2);
-
-		//if determinant is near zero, ray lies in plane of triangle
-
-		det = pvec.dot(edge1);
-
-		if(det > (-1*EPSILON) && det < EPSILON){
-			return null;
-		}
-		inv_det = 1.0/det;
-
-		//calculate the distance from vertex 0 to ray origin
-
-		tvec = new Vector3d(r.origin.x - vertices.get(0).x, r.origin.y - vertices.get(0).y,
-				r.origin.z - vertices.get(0).z);
-
-		//calculate u and test bounds
-
-		u = pvec.dot(tvec) * inv_det;
-
-		if( u < 0.0 || u > 1.0 ){
-
-			return null;
-		}
-		
-		//prepare to test v
-		qvec.cross(tvec, edge1);
-
-		//calculate v and test bounds
-		v = qvec.dot(r.direction) * inv_det;
-
-		if( v < 0.0 || (u + v) > 1.0){
-			return null;
-		}
-
-		//calculate t, ray intersects triangle
-		t = qvec.dot(edge2) * inv_det;
-
-		double xintersect = r.origin.x + (r.direction.x * t);
-		double yintersect = r.origin.y + (r.direction.y * t);
-		double zintersect = r.origin.z + (r.direction.z * t);
-
-		return new Point3d(xintersect, yintersect, zintersect);
-
+	public Point3d intersect(Ray r) 
+	{
+	    double ntd = normal.dot(r.direction);
+        if ( ntd > 0 ) {
+            return null;
+        } else {
+            Vector3d rOriginVec = new Vector3d(r.origin.x, r.origin.y, r.origin.z);
+            Vector3d p1 = new Vector3d(vertices.get(0).x, vertices.get(0).y, vertices.get(0).z);
+            double t = -(normal.dot(rOriginVec) - normal.dot(p1)) / ntd;
+            Point3d poi = new Point3d(r.origin.x+t*r.direction.x, r.origin.y+t*r.direction.y, r.origin.z+t*r.direction.z);
+            Vector3d l1 = new Vector3d(vertices.get(0).x-poi.x, vertices.get(0).y-poi.y, vertices.get(0).z-poi.z);
+            l1.normalize();
+            Vector3d l2 = new Vector3d(vertices.get(1).x-poi.x, vertices.get(1).y-poi.y, vertices.get(1).z-poi.z);
+            l2.normalize();
+            Vector3d l3 = new Vector3d(vertices.get(2).x-poi.x, vertices.get(2).y-poi.y, vertices.get(2).z-poi.z);
+            l3.normalize();
+            double total = Math.acos(l1.dot(l2)) + Math.acos(l2.dot(l3)) + Math.acos(l3.dot(l1));
+            double absPiTotal = 2*Math.PI - total;
+            if (absPiTotal < 0) {
+                absPiTotal *= -1;
+            }
+            if(absPiTotal < .0001){ //point is inside plane
+                return poi;
+            } else { //point is outside plane
+                return null;
+            }
+        }
 	}
 	
 	public Color getColor(Point3d point) {
