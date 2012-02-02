d528d0d8255a7a68e93b2fa185153c9e0ee471c7
Sam Milton <srm2997@rit.edu>
2012-02-01 22:49:58 -0500

    Updated WorldAntimatter to render dynamically, not from a file

--- src/WorldAntimatter.java
+++ src/WorldAntimatter.java
@@ -10,15 +10,6 @@ import javax.vecmath.Vector3d;
 
 public class WorldAntimatter extends WorldGenerator{
 	private World[] worlds;
-	
-    // Charge on an antiproton.
-    static final double QP = 3.0;
-
-    // Magnetic field strength.
-    static final double B = 3.0;
-
-    static final double QP_QP = QP * QP;
-    static final double QP_B = QP * B;
     
 	
 	final static double back = -10;
@@ -28,34 +19,13 @@ public class WorldAntimatter extends WorldGenerator{
 	final static double bottom = 0.0;
 	final static double top = 3.0;
 	
-	Vector3d pos[];
-	Vector3d spd[];
-	Vector3d acc[];
-	Vector3d temp = new Vector3d();
-	// Total momentum.
-	static Vector3d totalMV = new Vector3d();
-	
-	double dt = 0.00001;
-	double one_half_dt_sqr = 0.5 * dt * dt;
-	
-	double posx[];
-	double posy[];
-	double posz[];
-	
-	double spdx[];
-	double spdz[];
-	
 	public WorldAntimatter( int seconds, int particles ){
-		this( seconds, particles, -1 );
+		this( seconds, particles, new Random().nextInt() );
 	}
 	
-	public WorldAntimatter( int seconds, int particles, int seed ){
+	public WorldAntimatter( int frames, int particles, int seed ){
 		LinkedList<World> worldList = new LinkedList<World>();
-		if( seed < 0 ){
-			seed = new Random().nextInt();
-		}
-		Random r = new Random( seed );
-		System.out.println( "WorldAntimatter( " + seconds + ", " + particles + ", " + seed + " )" );
+		System.out.println( "WorldAntimatter( " + frames + ", " + particles + ", " + seed + " )" );
 		
 		double centerx = 0;
 		double centerz = -2;
@@ -70,34 +40,8 @@ public class WorldAntimatter extends WorldGenerator{
 		double kr = 0.3;
 		double kt = 0.5;
 		int ke = 50;
-
-		// Marble x,y,z positions
-		posx = new double[particles];
-		posy = new double[particles];
-		posz = new double[particles];
-		
-		spdx = new double[particles];
-		spdz = new double[particles];
-		
-		pos = new Vector3d[particles];
-		spd = new Vector3d[particles];
-		acc = new Vector3d[particles];
 		
 		for( int i = 0; i < particles; i++ ){
-			posx[i] = r.nextDouble() * ( right - left ) + left;
-			posy[i] = centery;
-			posz[i] = r.nextDouble() * ( front - back ) + back;
-			System.out.println( posx[i] + " " + posy[i] + " " + posz[i] );
-//			pos[i] = new Vector3d( posx[i], posy[i], posz[i] );
-			pos[i] = new Vector3d( 0, posy[i], -5 );
-			
-			spdx[i] = r.nextDouble() * 5;
-			spdz[i] = r.nextDouble() * 5;
-			spd[i] = new Vector3d( spdx[i], 0, spdz[i] );
-//			spd[i] = new Vector3d( spdx[i], 0, spdz[i] );
-			
-			acc[i] = new Vector3d( 0, 0, 0 );
-			
 			if( i % 2 == 0 ){
 				colors[i] = new Color(255,20,147); // Pink
 			}else{
@@ -106,10 +50,10 @@ public class WorldAntimatter extends WorldGenerator{
 		}
 		
 		// Set up floor data
-		Point3d PLANEVERTLF = new Point3d( left, bottom, front );
-		Point3d PLANEVERTRF = new Point3d( right, bottom, front );
-		Point3d PLANEVERTRR = new Point3d( right, bottom, back );
-		Point3d PLANEVERTLR = new Point3d( left, bottom, back );
+		Point3d PLANEVERTLF = new Point3d( left*2, bottom, front );
+		Point3d PLANEVERTRF = new Point3d( right*2, bottom, front );
+		Point3d PLANEVERTRR = new Point3d( right*2, bottom, back );
+		Point3d PLANEVERTLR = new Point3d( left*2, bottom, back );
 
 		ArrayList<Point3d> triAVertices = new ArrayList<Point3d>();
 		ArrayList<Point3d> triBVertices = new ArrayList<Point3d>();
@@ -121,24 +65,18 @@ public class WorldAntimatter extends WorldGenerator{
 		triBVertices.add( PLANEVERTLF );
 		triBVertices.add( PLANEVERTRR );
 		triBVertices.add( PLANEVERTLR );
+
+		AntiprotonSeq.simulate( seed, frames, particles );
+		double[][] points = AntiprotonSeq.positions;
 		
-		int frame = 0;
-		BufferedReader br = null;
-		try{
-			br = new BufferedReader( new FileReader("antiprotons") );
-		
-//			while( frame < 24 * seconds ){
-			while( br.ready() ){
+		for( int i = 0; i < frames; i++ ){
 				World world = new World();
 				worldList.add( world );
 				
 				// Set up spheres
-				Point3d centers[] = new Point3d[posx.length];
+				Point3d centers[] = new Point3d[particles];
 				for( int k = 0; k < centers.length; k++ ){
-					String[] vals = br.readLine().trim().split(" ");
-//					centers[k] = new Point3d( posx[k], posy[k], posz[k] );
-//					centers[k] = new Point3d( pos[k].x, pos[k].y, pos[k].z );
-					centers[k] = new Point3d( ( Double.parseDouble(vals[0]) - 5 ) * .75, 0.25, -Double.parseDouble(vals[1]) * .75 );
+					centers[k] = new Point3d( ( points[i*particles+k][0] - 5 ) * .75, 0.25, -points[i*particles+k][1] * .75 );
 				}
 				
 				// Set up light
@@ -152,105 +90,11 @@ public class WorldAntimatter extends WorldGenerator{
                 world.add( new Triangle( triBVertices, new Color( 255, 255, 255 ), 0.2, 0.4, 0.6, 20, 0.6, 0.0, 1 ) );
 		
 				world.add( new PointLight( LIGHTCENTER, new Color( 255.0, 255.0, 255.0 ) ) );
-				
-				
-//				for( int step = 0; step < 100; step++ ){
-//					// Update particle positions
-//					computeAcceleration( centers );
-//					step( centers );
-//				}
-//				
-//				// Compute total momentum.
-//				computeTotalMomentum( centers );
-		
-				
-				frame++;
 			}
-
-		}catch( Exception e ){
-			e.printStackTrace();
-			System.err.println( e.getMessage() );
-		}
 
 		worlds = worldList.toArray( new World[0] );
 	}
-	
-	private void computeAcceleration( Point3d[] spheres ){
-		// Accumulate forces between each pair of antiprotons, but not between an antiproton and itself.
-		for( int i = 0; i < spheres.length; ++ i ){
-			
-			Vector3d a_i = acc[i];
-            Vector3d p_i = pos[i];
-            
-		    for (int j = 0; j < i; ++ j){
-		    	temp = new Vector3d( p_i );
-		        temp.sub( pos[j] );
-		        
-		        double dsqr = temp.lengthSquared();
-		        temp.scale( QP_QP / ( dsqr * Math.sqrt(dsqr) ) );
-		        a_i.add( temp );
-		    }
-		    for (int j = i+1; j < spheres.length; ++ j){
-		        temp = new Vector3d( p_i );
-		        temp.sub( pos[j] );
-		        double dsqr = temp.lengthSquared();
-		        temp.scale( QP_QP / ( dsqr * Math.sqrt(dsqr) ) );
-		        a_i.add( temp );
-		    }
-		}
-    }
-	
-	private void step( Point3d[] spheres ){
-		// Move all antiprotons.
-		for( int i = 0; i < spheres.length; ++ i ){
-			Vector3d a_i = acc[i];
-			Vector3d v_i = spd[i];
-			Vector3d p_i = pos[i];
-			
-			// Accumulate acceleration on antiproton from magnetic field.
-			temp = new Vector3d( v_i );
-			temp.scale( QP_B );
-			temp = new Vector3d( temp.z, temp.y, -temp.x ); // rotate 270
-			a_i.add( temp );
-			
-			// Update antiproton's position and velocity.
-			temp = new Vector3d( v_i );
-			temp.scale(dt);
-			p_i.add( temp );
-			
-			temp = new Vector3d( a_i );
-			temp.scale( one_half_dt_sqr );
-			p_i.add( temp );
-			
-			temp = new Vector3d( a_i );
-			temp.scale( dt );
-			v_i.add( temp );
-			
-			// Clear antiproton's acceleration for the next step.
-			a_i = new Vector3d();
-		}
-	}
-	
-	/**
-     * Compute the total momentum for all the antiprotons. The answer is stored
-     * in <TT>totalMV</TT>.
-     */
-    private void computeTotalMomentum( Point3d sphere[] ){
-		totalMV = new Vector3d();
-		for( int i = 0; i < sphere.length; ++ i ){
-			totalMV.add( spd[i] );
-		}
-	}
 
-
-	
-	private static double distance( double x, double y, double z, Point3d sphere ){
-		double dx = x - sphere.x;
-		double dy = y - sphere.y;
-		double dz = z - sphere.z;
-		return Math.sqrt( x*x + y*y + z*z );
-	}
-			
 	public World[] getWorlds() {
 		return worlds;
 	}
