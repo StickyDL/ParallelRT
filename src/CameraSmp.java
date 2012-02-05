d24b570733e76cc69f1a5d9ef956b7e802d5ac75
Sam Milton <srm2997@rit.edu>
2012-02-04 19:08:09 -0500

    Added Smp2, which is parallelized on pixels

---
+++ src/CameraSmp.java
@@ -0,0 +1,337 @@
+import javax.imageio.ImageIO;
+import javax.swing.JProgressBar;
+import javax.vecmath.Point3d;
+import javax.vecmath.Vector3d;
+
+import edu.rit.pj.IntegerForLoop;
+import edu.rit.pj.IntegerSchedule;
+import edu.rit.pj.ParallelForLoop;
+import edu.rit.pj.ParallelRegion;
+import edu.rit.pj.ParallelTeam;
+
+import java.awt.image.BufferedImage;
+import java.awt.image.WritableRaster;
+import java.io.File;
+import java.util.Random;
+
+public class CameraSmp {
+
+	private final double XMIN = -0.5;
+	private final double XMAX = 0.5;
+	private final double YMIN = 0.5;
+	private final double YMAX = 1.5;
+	private double n = 1.0;
+	private final int XRES = 500;
+	private final int YRES = 500;
+	private final double Z = 0.5;
+
+	BufferedImage image;
+
+	private int MAX_DEPTH = 5;
+	Random rand = new Random();
+	private double deltaY = (YMAX - YMIN) / YRES;
+	private double deltaX = (XMAX - XMIN) / XRES;
+	private double[] pixelArray;
+	private double BACKGRD_RED = 220;
+	private double BACKGRD_GREEN = 220;
+	private double BACKGRD_BLUE = 255;
+	World w;
+	Point3d position;
+	Point3d lookat;
+	Vector3d up;
+	
+	private int xstart = 0;
+	private int xfinish = XRES;
+	private int ystart = 0;
+	private int yfinish = YRES;
+
+	public CameraSmp(Point3d position, Point3d lookat, Vector3d up){
+
+		this.position = position;
+		this.lookat = lookat;
+		this.up = up;
+
+		image = new BufferedImage(XRES, YRES, BufferedImage.TYPE_INT_RGB);
+		pixelArray = new double[XRES*YRES*3];
+
+	}
+	
+	public CameraSmp( Point3d position, Point3d lookat, Vector3d up, int xmin, int xmax, int ymin, int ymax ){
+		xstart = xmin;
+		xfinish = xmax;
+		ystart = ymin;
+		yfinish = ymax;
+		
+	}
+
+	public void render(World w, File outputFile, JProgressBar progress ) throws Exception {
+		if( progress != null ){
+			progress.setMaximum( XRES * YRES );
+		}
+		int pixel_count = 0;
+		
+		
+		WritableRaster raster = image.getRaster();
+		this.w = w;
+		final Point3d camPoint = this.position;
+		
+
+		new ParallelTeam(8).execute( new ParallelRegion(){
+			public void run() throws Exception{
+				execute( 0, YRES - 1, new IntegerForLoop(){
+					int pixelNum = 0;
+					double y;
+					
+					// Extra padding
+					long p0, p1, p2, p3, p4, p5, p6, p7;
+                    long p8, p9, pa, pb, pc, pd, pe, pf;
+					
+//					public IntegerSchedule schedule(){
+//						return IntegerSchedule.guided();
+//					}
+					
+					public void run( int low, int high ) throws Exception{
+						long start = System.currentTimeMillis();
+						
+						pixelNum = low * XRES * 3;
+						int renders = 0;
+						
+						y = YMAX - ( low * deltaY );
+						
+						// low + 1 is wrong, should be +0. Helps see the sections on the image
+						for( int row = low + 1; row <= high; row++ ){
+							for( double x = XMIN; x < XMAX; x = x + deltaX) {
+								renders++;
+
+								Ray rtRay = new Ray( camPoint, new Vector3d(x-(camPoint.x), y-(camPoint.y), Z-(camPoint.z)));	
+							
+								//Illuminate the pixel given the ray
+								Color pixelColor = illuminate(rtRay, 1);
+								
+								pixelArray[pixelNum] = pixelColor.r;
+								pixelArray[pixelNum+1] = pixelColor.g;
+								pixelArray[pixelNum+2] = pixelColor.b;
+									
+								if(pixelArray[pixelNum] > 255)
+									pixelArray[pixelNum] = 255;
+								if(pixelArray[pixelNum+1] > 255)
+									pixelArray[pixelNum+1] = 255;
+								if(pixelArray[pixelNum+2] > 255)
+									pixelArray[pixelNum+2] = 255;
+								pixelNum += 3;
+									
+//								pixel_count++;
+							}
+//							if( progress != null ){
+//								progress.setValue( pixel_count );
+//							}
+							y = y - deltaY;
+						}
+						System.out.println( low + " - " + high + " Done.\t" + ( System.currentTimeMillis() - start) + "ms\twith " + renders + " renders");
+					}
+				});
+			}
+		} );
+		
+		raster.setPixels(0, 0, XRES, YRES, pixelArray);
+		
+		try{
+			ImageIO.write(image, "png", outputFile );
+		}catch( Exception e ){
+			System.err.println( e.getMessage() );
+			e.printStackTrace();
+		}
+
+	}
+	
+	public Color illuminate(Ray r, int depth){
+		Color lightF = new Color(0,0,0);
+		Color light = new Color(0,0,0);
+		int objIndex = -1;
+		Point3d iPoint = new Point3d(Double.MAX_VALUE, Double.MAX_VALUE, Double.MAX_VALUE);
+		//Find the object closest to the Origin of the Ray
+		for( int i=0; i<w.objectList.size(); i++) {
+			Point3d inter = w.objectList.get(i).intersect(r);
+			if( inter != null) {
+				if( r.origin.distance(inter) < r.origin.distance(iPoint)) {
+					//System.out.println("(" + inter.x + ", " + inter.y + ", " + inter.z + ")");
+					iPoint = inter;
+					objIndex = i;
+				}
+			}
+		}
+		//If no object was found, set color to background
+		if( iPoint.x == Double.MAX_VALUE && iPoint.y == Double.MAX_VALUE && iPoint.z == Double.MAX_VALUE )
+			lightF = new Color(BACKGRD_RED, BACKGRD_GREEN, BACKGRD_BLUE);
+		//Else determine color for object
+		else {
+			Vector3d N = w.objectList.get(objIndex).normal;
+			N.normalize();
+			//Retrieve object constants - Checkpoint 3
+			double ka = w.objectList.get(objIndex).ka;
+			double kd = w.objectList.get(objIndex).kd;
+			double ks = w.objectList.get(objIndex).ks;
+			double ke = w.objectList.get(objIndex).ke;
+			double kr = w.objectList.get(objIndex).kr;
+			double kt = w.objectList.get(objIndex).kt;
+					
+			//Checkpoint 4 - Get color. If object has a shader, returns color from that.
+			Color objectColor = w.objectList.get(objIndex).getColor(iPoint);
+				
+			//Calculate Ambient Light
+			light = new Color(
+					ka * ((w.ambRed * objectColor.r)/255),
+					ka * ((w.ambGreen * objectColor.g)/255), 
+					ka * ((w.ambBlue * objectColor.b)/255));
+				
+				
+			//create ray from light source to point of intersection
+			for( int lightIndex = 0; lightIndex < w.lightList.size(); lightIndex++ ) {
+			Ray shadowRay = new Ray(iPoint, new Vector3d(w.lightList.get(lightIndex).position.x - iPoint.x, 
+							w.lightList.get(lightIndex).position.y - iPoint.y, w.lightList.get(lightIndex).position.z - iPoint.z));
+				
+			//check to see if shadow ray intersects any object.
+			Point3d lightInter = null;
+				
+			for( int j=0; j<w.objectList.size(); j++) {
+				if( objIndex != j ) {
+					Point3d shadowInter = w.objectList.get(j).intersect(shadowRay);
+				
+					if( shadowInter != null && (iPoint.distance(w.lightList.get(lightIndex).position) > shadowInter.distance(w.lightList.get(lightIndex).position))){
+						if(lightInter == null || (lightInter.distance(w.lightList.get(lightIndex).position) > shadowInter.distance(w.lightList.get(lightIndex).position))){
+							lightInter = shadowInter;
+						}
+					}
+				}
+			}
+				
+			//If Point is not in shadow, continue finding shading
+			if(lightInter == null){
+				//calculating V and S
+				Vector3d V = new Vector3d((this.position.x - iPoint.x), (this.position.y - iPoint.y),
+								(this.position.z - iPoint.z));
+				Vector3d S = new Vector3d(w.lightList.get(lightIndex).position.x-(iPoint.x), 
+								w.lightList.get(lightIndex).position.y-(iPoint.y), 
+								w.lightList.get(lightIndex).position.z-(iPoint.z));
+					
+				//Normalizing vectors
+				V.normalize();
+				S.normalize();
+					
+				//calculating S dot product N
+				double sdotn = S.dot(N);
+					
+				if( sdotn > 0 ) {
+					//Diffuse color
+					Color diffuseColor = new Color( (kd * sdotn) * ((w.lightList.get(lightIndex).red * objectColor.r )/255), 
+									(kd * sdotn) * ((w.lightList.get(lightIndex).green * objectColor.g)/255), 
+									(kd * sdotn) * ((w.lightList.get(lightIndex).blue * objectColor.b)/255));
+					
+					light.r += diffuseColor.r;
+					light.g += diffuseColor.g;
+					light.b += diffuseColor.b;
+					
+					double fraction = (2*sdotn);
+				
+					Vector3d newN = new Vector3d(N.x * fraction, N.y * fraction, N.z * fraction);
+					Vector3d R = new Vector3d(S.x - newN.x, S.y - newN.y, S.z - newN.z);
+					R.normalize();
+					
+					double sdotr = V.dot(R);
+						
+					Color specColor = new Color( (ks * Math.pow(sdotr, ke)) * ((w.lightList.get(lightIndex).red * w.objectList.get(objIndex).speccolor.r)/255), (ks * Math.pow(sdotr, ke)) *  ((w.lightList.get(lightIndex).green * w.objectList.get(objIndex).speccolor.g)/255), 
+										(ks * Math.pow(sdotr, ke)) * ((w.lightList.get(lightIndex).blue * w.objectList.get(objIndex).speccolor.b)/255));
+					
+					light.r += specColor.r;
+					light.g += specColor.g;
+					light.b += specColor.b;
+				}
+			}
+				
+			//Determine Reflection and Transmission (Recursively) - Checkpoint 5 & 6
+			if( depth < MAX_DEPTH){
+				//Reflection
+				if(kr > 0){
+					Vector3d D = new Vector3d(r.origin.x-iPoint.x, 
+									r.origin.y-iPoint.y, r.origin.z-iPoint.z);
+					D.normalize();
+					double ddotn = D.dot(N);
+					if(ddotn >= 0){
+						Vector3d newN = new Vector3d(N.x * (2*ddotn), N.y * (2*ddotn), N.z * (2*ddotn));
+						//newN.normalize();
+						Vector3d R = new Vector3d( (newN.x - D.x), (newN.y - D.y), (newN.z - D.z));
+						//R.normalize();
+						//System.out.println("Angle i: " + Math.acos(D.dot(N)));
+						//System.out.println("Angle r: " + Math.acos(R.dot(N)));
+						Ray reflectiveRay = new Ray(iPoint, R);
+						Color reflect = illuminate(reflectiveRay, depth+1);
+						light.r += kr*reflect.r;
+						light.g += kr*reflect.g;
+						light.b += kr*reflect.b;
+						
+					}	
+				}
+				//Transmission
+				if(kt > 0.0) {
+					Vector3d D = new Vector3d(iPoint.x - r.origin.x, iPoint.y - r.origin.y, iPoint.z - r.origin.z);
+					D.normalize();
+					Vector3d negD = new Vector3d(-D.x, -D.y, -D.z);
+					double nit;
+					if( depth % 2 == 1 ) {
+						nit = this.n / w.objectList.get(objIndex).n;
+					}
+					else {
+						nit = w.objectList.get(objIndex).n / this.n;
+						N = new Vector3d(-N.x, -N.y, -N.z);
+					}
+					double alpha = nit;
+					double underSqRt = 1.0 + ((nit*nit) * ((negD.dot(N) * negD.dot(N)) - 1.0));
+					if( underSqRt > 0.0 ) {
+						double beta = (nit * (negD.dot(N))) - Math.sqrt(underSqRt);
+						Vector3d T = new Vector3d(alpha*D.x+beta*N.x, alpha*D.y + beta*N.y, alpha*D.z + beta*N.z);
+						T.normalize();
+						Ray transmissionRay = new Ray(iPoint, T);
+						Color transmit = illuminate(transmissionRay, depth+1);
+						light.r += kt*transmit.r;
+						light.g += kt*transmit.g;
+						light.b += kt*transmit.b;
+					}
+					
+					else {
+						//Calculate Reflection Ray Instead
+						D = new Vector3d(r.origin.x-iPoint.x, r.origin.y-iPoint.y, r.origin.z-iPoint.z);
+						D.normalize();
+						double ddotn = D.dot(N);
+						if(ddotn >= 0){
+							Vector3d newN = new Vector3d(N.x * (2*ddotn), N.y * (2*ddotn), N.z * (2*ddotn));
+//							Vector3d R = new Vector3d( (newN.x - D.x), (newN.y - D.y), (newN.z - D.z));
+							Ray reflectiveRay = new Ray(iPoint, new Vector3d( (newN.x - D.x), (newN.y - D.y), 
+												(newN.z - D.z)));
+							Color reflect = illuminate(reflectiveRay, depth+1);
+							light.r += kt*reflect.r;
+							light.g += kt*reflect.g;
+							light.b += kt*reflect.b;
+						
+						}
+						
+					}
+					
+					
+				}
+			}
+			if( lightIndex > 0 ) {
+				lightF.r = (lightF.r + light.r) / 2;
+				lightF.g = (lightF.g + light.g) / 2;
+				lightF.b = (lightF.b + light.b) / 2;
+			}
+			else {
+				lightF.r = light.r;
+				lightF.g = light.g;
+				lightF.b = light.b;
+			}
+			}
+		}		
+		return lightF;
+	}
+
+}
---
+++ src/RayTracerSmp2.java
@@ -0,0 +1,172 @@
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JProgressBar;
+import javax.vecmath.Point3d;
+import javax.vecmath.Vector3d;
+
+import edu.rit.pj.BarrierAction;
+import edu.rit.pj.IntegerForLoop;
+import edu.rit.pj.IntegerSchedule;
+import edu.rit.pj.ParallelRegion;
+import edu.rit.pj.ParallelTeam;
+import edu.rit.pj.reduction.ObjectOp;
+import edu.rit.pj.reduction.SharedObject;
+
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.io.File;
+import java.util.Random;
+
+public class RayTracerSmp2{
+	final static Point3d CAMERACENTER = new Point3d( 0.0, 1.0, 2.0 );
+	final static Point3d CAMERALOOKAT = new Point3d( 0.0, 0.0, -1.0 ); // Direction
+	final static Vector3d CAMERAUP = new Vector3d( 0.0, 1.0, 0.0 );
+	
+	final static int THREADS = ParallelTeam.getDefaultThreadCount();
+	
+	private WorldGenerator genWorlds;
+
+	public static void main( String[] args ) throws Exception{
+		int seed = new Random().nextInt();
+		try{
+			seed = Integer.parseInt(args[0]);
+		}catch( Exception e ){}
+		
+		System.out.println( "Seed: " + seed );
+		
+//        RayTracerSmp rt = new RayTracerSmp( new WorldMarbles( 5, 10, true, seed ) );
+//		RayTracerSmp2 rt = new RayTracerSmp2( new WorldAntimatter( 24*20, 20, seed ) );
+		RayTracerSmp2 rt = new RayTracerSmp2( new WorldAntimatter( 1, 10, 1234567890 ) );
+        // RayTracerSmp rt = new RayTracerSmp( new WorldMarbleGrid( 10, WorldMarbleGrid.DIAGONAL ) );
+        rt.cleanup();
+        long startTime = System.currentTimeMillis();
+		rt.render();
+		System.out.println("Time: " + (System.currentTimeMillis() - startTime) + "msec");
+
+//		PlayMovie.main( new String[]{} );
+	}
+
+	/**
+	 * Create a ray tracer using the given world generator
+	 * 
+	 * @param genWorlds World generator
+	 */
+	public RayTracerSmp2( WorldGenerator genWorlds ){
+		this.genWorlds = genWorlds;
+	}
+	
+	public void cleanup(){
+		// Erase previous render
+		File[] files = new File( "." ).listFiles();
+		try{
+			for( int i = 0; i < files.length; i++ ){
+				if( files[i].getName().endsWith( ".png" ) ){
+					files[i].delete();
+				}
+			}
+		}catch( Exception e ){
+			System.exit( 1 );
+		}
+	}
+	
+	/**
+	 * Renders the Worlds produced by the world Generator
+	 * 
+	 * @throws Exception
+	 */
+	public void render() throws Exception{
+		// Setup worlds to render
+		final World[] worlds = genWorlds.getWorlds();
+
+		// Setup progress window
+		JFrame frame = new JFrame( "Rendering..." );
+		frame.setBounds( 0, 0, 300, 50 * THREADS + 50 );
+		frame.setLayout( new BorderLayout() );
+		frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+
+		// Create progress bars
+		final JProgressBar main = new JProgressBar( 0, worlds.length );
+		final JProgressBar[] bars = new JProgressBar[THREADS];
+		for( int i = 0; i < bars.length; i++ ){
+			bars[i] = new JProgressBar();
+			bars[i].setValue( 0 );
+			bars[i].setStringPainted( true );
+		}
+		main.setValue( 0 );
+		main.setStringPainted( true );
+		main.setString( "Rendering" );
+
+		// Setup left panel, with progress bar labels
+		JPanel left = new JPanel();
+		left.setLayout( new GridLayout( THREADS + 1, 1 ) );
+		left.add( new JLabel( "Total:" ) );
+		for( int i = 0; i < bars.length; i++ ){
+			left.add( new JLabel( "Frame:" ) );
+		}
+
+		// Setup right panel, with progress bars
+		JPanel right = new JPanel();
+		right.setLayout( new GridLayout( THREADS + 1, 1 ) );
+		right.add( main );
+		for( int i = 0; i < bars.length; i++ ){
+			right.add( bars[i] );
+		}
+
+		// Finalize layout and display
+		frame.add( left, BorderLayout.WEST );
+		frame.add( right, BorderLayout.CENTER );
+//		frame.setVisible( true );
+
+		final SharedObject<JProgressBar> sharedMain = new SharedObject<JProgressBar>( main );
+
+		ParallelTeam calcTeam = new ParallelTeam();
+		
+		long start = System.currentTimeMillis();
+		// Render all worlds and save to file
+		for( int i = 0; i < worlds.length; i++ ){
+			CameraSmp camera = new CameraSmp( CAMERACENTER, CAMERALOOKAT, CAMERAUP );
+			camera.render( worlds[i], new File( "render_" + String.format( "%1$04d" , i) + ".png" ), null );
+		}
+		long fin = System.currentTimeMillis();
+		System.out.println( ( fin - start ) + " seconds" );
+		
+//		new ParallelTeam().execute( new ParallelRegion(){
+//			public void run() throws Exception{
+//				final ObjectOp<JProgressBar> op = new ObjectOp<JProgressBar>(){
+//					public JProgressBar op( JProgressBar arg0, JProgressBar arg1 ){
+//						arg0.setValue( arg0.getValue() + 1 );
+//						return arg0;
+//					}
+//				};
+//
+//				execute( 0, worlds.length - 1, new IntegerForLoop(){
+//
+//					public IntegerSchedule schedule()
+//					{
+//						return IntegerSchedule.guided();
+//					}
+//
+//					public void run( int low, int high ) throws Exception{
+//						Camera camera = new Camera( CAMERACENTER, CAMERALOOKAT, CAMERAUP );
+//						int index = getThreadIndex();
+//
+//						// Render all worlds and save to file
+//						for( int i = low; i <= high; i++ ){
+//							bars[index].setString( "Frame " + i );
+//							bars[index].setValue( 0 );
+//							camera.render( worlds[i], new File( "render_" + String.format( "%1$04d", i ) + ".png" ), bars[index] );
+//							sharedMain.reduce( null, op );
+//						}
+//
+//						bars[index].setString( "Done!" );
+//					}
+//				},
+//						BarrierAction.NO_WAIT );
+//			}
+//		} );
+
+		main.setString( "Done!" );
+	}
+
+}
