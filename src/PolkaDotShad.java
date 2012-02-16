7174e69a7e24ad94baa6a06d6cf281955ffd4742
Steven Glazer <sfg6126@rit.edu>
2012-02-16 17:07:05 -0500

    Commented PolkDotShad.java

--- src/PolkaDotShad.java
+++ src/PolkaDotShad.java
@@ -2,12 +2,33 @@ import javax.vecmath.Point3d;
 import java.util.Random;
 import java.util.ArrayList;
 
+/**
+ * PolkaDotShad class is a type of shader that can be applied to graphic objects.
+ * It gives the appearance of a tri-color polka-dotting on the object.
+ * This class is only accurately used on horizontal triangles at the moment.
+ *
+ * @author  Steve Glazer
+ * @author  Sara Jackson
+ * @author  Sam Milton
+ * @version 16-Feb-2012
+ */
 public class PolkaDotShad extends Shader {
 	ArrayList<Color> colors;
 	double polkaDist, polkaRad;
 	Point3d bound;
 	Random rand;
 	
+	/**
+     * Constructor
+     *
+     * @param origin            the origin of the shader
+     * @param bound             bound of the shader
+     * @param color1            the primary color of the shader
+     * @param color2            color for half the polka-dots
+     * @param color3            color for the other half of the polka-dots
+     * @param polkaDist         distance between the polka-dots
+     * @param polkaRad          radius of the polka-dots
+     */
 	public PolkaDotShad(Point3d origin, Point3d bound, Color color1, Color color2, Color color3, double polkaDist, double polkaRad) {
 		super(origin);
 		this.bound = bound;
@@ -20,6 +41,15 @@ public class PolkaDotShad extends Shader {
 		this.rand = new Random();
 	}
 	
+	/**
+     * Retrieves the color of the shader at the given point.
+     * Determines the placement of the point on the shader to determine
+     * the color.
+     *
+     * @param point     the point to get that color of the shader at
+     *
+     * @return the color at the given point
+     */
 	public Color shade(Point3d point) {
 		Color result = null;
 		boolean even = true;
