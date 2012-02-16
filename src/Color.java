d4318fe2edfb1bd945f1a8368234f96a60cd5a7f
Steven Glazer <sfg6126@rit.edu>
2012-02-16 17:10:51 -0500

    Commented Color.java

--- src/Color.java
+++ src/Color.java
@@ -1,10 +1,19 @@
-
+/**
+ * Very basic Color class to keep track of colors for
+ * the RayTracers.
+ *
+ * @author  Steve Glazer
+ * @author  Sara Jackson
+ * @author  Sam Milton
+ * @version 16-Feb-2012
+ */
 public class Color {
 
 	double r;
 	double g;
 	double b;
 	
+	// Constructor given the rgb values
 	public Color( double r, double g, double b){
 		this.r = r;
 		this.g = g;
