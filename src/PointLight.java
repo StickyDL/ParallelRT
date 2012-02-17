9f9a30e2ffd48e9e09b48a09cb7152ffbea4bf11
Steven Glazer <sfg6126@rit.edu>
2012-02-17 09:37:27 -0500

    Commented PointLight.java

--- src/PointLight.java
+++ src/PointLight.java
@@ -1,10 +1,26 @@
 import javax.vecmath.Point3d;
 
+/**
+ * The PointLight class represents a point light in
+ * a 3d scene. A point light has a position and color
+ * which will light up the objects in the scene
+ *
+ * @author  Steve Glazer
+ * @author  Sara Jackson
+ * @author  Sam Milton
+ * @version 16-Feb-2012
+ */
 public class PointLight {
 
 	double red, green, blue;
 	Point3d position;
 
+    /**
+     * Constructor
+     *
+     * @param position      position of the point light
+     * @param c             color of the point light
+     */
 	public PointLight(Point3d position, Color c) {
 
 		this.position = position;
